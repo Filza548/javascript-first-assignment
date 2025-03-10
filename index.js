4beb48fa239160f1e97b090bdc3766848f492e3c
Filza548 <filzajameel458@gmail.com>
2025-03-11 00:14:12 +0500

    done

---
+++ index.js
@@ -0,0 +1,63 @@
+
+let number = +prompt("Ek number daalen:");
+
+
+    if (number % 2 ) {
+        alert("Aapne odd number dala hai!");
+        console.log("Aapne odd number dala hai!");
+        
+    } else {
+        alert("Aapne even number dala hai!");
+        console.log("Aapne even number dala hai!");
+        
+    }
+
+   let userName = prompt("Enter your name")
+   console.log(userName,  " Sucess");
+   alert("User name is sucessfull")
+ 
+   
+let email = prompt("Enter you email")
+console.log( email , "Email " );
+alert("your email is sucessfull")
+
+
+let password = "filzajameel12"
+let c = prompt("Enter Your Password")
+
+if(password === c){
+    console.log("correct Password");
+    alert ("correct Password")
+}else{
+    console.log("Incorrect password");
+    alert("Incorrect password")
+}
+
+let contact = +prompt("Enter Your number")
+console.log("Your number is sucessfull ",  contact);
+alert("Your number is sucessfull")
+
+let toProm = +prompt("Enter Your Number & covert your result")
+let english = 18
+let urdu = 15
+let math = 23
+let science = 17
+
+let total = english + urdu + math + science 
+
+let totalMarks = 130
+
+console.log(total/totalMarks*100 , "Subject" );
+console.log(toProm/totalMarks*100 , "user");
+
+
+
+
+ 
+
+
+
+
+
+
+
